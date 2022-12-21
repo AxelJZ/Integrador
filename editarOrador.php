927c399eab830b9728ed0d43d50fc49163f124aa
JonathanZ <jonathanz76@gmail.com>
2022-12-21 12:07:28 -0300

    Actualización v1.0.21

--- editarOrador.php
+++ editarOrador.php
@@ -1,12 +1,16 @@
 <?php
+echo "<pre>";
+var_dump($_POST);
+echo "<pre>";
+
 include("conexion.php");
 
 $conexion = conexion();
 
-$id = $_GET["id"];
+$id = $_POST["id"];
 $nombre = $_POST["nombre"];
 $apellido = $_POST["apellido"];
-$mail = $_POST["email"];
+$email = $_POST["email"];
 
 $sql = "UPDATE inscriptos SET nombre='$nombre', apellido='$apellido', email='$email' WHERE id=$id";
 
--- formEditarOrador.php
+++ formEditarOrador.php
@@ -9,7 +9,7 @@ $id = $_GET["id"];
 
 $sql = "SELECT * FROM inscriptos WHERE id=$id";
 $query = mysqli_query($conexion, $sql);
-$fila = mysqli_fetch_array($query);
+$fila = mysqli_fetch_assoc($query);
 
 ?>
 
@@ -18,19 +18,19 @@ $fila = mysqli_fetch_array($query);
             <h4 class="text-white pb-4 text-center">Editar Datos</h4>
             <form action="editarOrador.php" method="POST" class="row g-3">
                 <div class="col d-none">
-                    <input type="text" class="" id="id" name="id" value="<?php $fila["id"]?>">
+                    <input type="text" class="" id="id" name="id" value="<?php $fila["id"]?>" hidden>
                 </div>
                 <div class="">
-                    <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre" value="<?php echo $fila["nombre"]?>">
+                    <input type="text" class="form-control" id="nombre" name="nombre" value="<?php echo $fila["nombre"]?>">
                 </div>
                 <div class="">
-                    <input type="text" class="form-control" id="apellido" name="apellido" placeholder="Apellido" value="<?php echo $fila["apellido"]?>">
+                    <input type="text" class="form-control" id="apellido" name="apellido" value="<?php echo $fila["apellido"]?>">
                 </div>
                 <div class="">
-                    <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="<?php echo $fila["email"]?>" pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}" size="30" required">
+                    <input type="text" class="form-control" id="email" name="email" value="<?php echo $fila["email"]?>" pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}" size="30" required">
                 </div>
                 <div class="">
-                    <button type="submit" value="editar" name="editar" class="btn btn-primary w-100">Modificar</button>
+                    <button type="submit" class="btn btn-primary w-100">Modificar</button>
                 </div>
                 
             </form>
